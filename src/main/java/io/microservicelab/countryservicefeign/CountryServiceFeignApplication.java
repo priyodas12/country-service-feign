aa40ef755750393e17ad65f42e18f9e511abe3fd
Priyobrato Das <priyodas12@gmail.com>
2020-08-26 00:10:12 +0530

    Initial commit

---
+++ src/main/java/io/microservicelab/countryservicefeign/CountryServiceFeignApplication.java
@@ -0,0 +1,13 @@
+package io.microservicelab.countryservicefeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryServiceFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryServiceFeignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/microservicelab/countryservicefeign/CountryServiceFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package io.microservicelab.countryservicefeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryServiceFeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
